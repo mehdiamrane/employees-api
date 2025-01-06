6155325da2a152c1ff8b4267c34c89ed310b6062
Mehdi Amrane <mehdi@amrane.fr>
2025-01-06 03:05:48 +0100

    Update '@faker-js/faker' dependency to version 8.4.1

--- src/utils/generators.ts
+++ src/utils/generators.ts
@@ -9,7 +9,7 @@ export function generateEmployees(count: number): Employee[] {
       name: faker.person.fullName(),
       salary: faker.helpers.rangeToNumber({ min: 30000, max: 150000 }),
       age: faker.helpers.rangeToNumber({ min: 20, max: 65 }),
-      profile_image: faker.image.avatar(),
+      profile_image: faker.image.avatarGitHub(),
     });
   }
   return employees;
